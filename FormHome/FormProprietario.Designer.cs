c91a64e43a49d33b6339c6a59452a84abea796b8
Alexandre de Lima <allima1991@gmail.com>
2015-11-15 21:23:22 -0200

    s

--- FormHome/FormProprietario.Designer.cs
+++ FormHome/FormProprietario.Designer.cs
@@ -60,7 +60,7 @@
             this.dataGridView1.DataSource = this.proprietarioBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(795, 347);
+            this.dataGridView1.Size = new System.Drawing.Size(795, 249);
             this.dataGridView1.TabIndex = 0;
             // 
             // masterimovelDataSet
