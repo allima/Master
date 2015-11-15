d5deeabd6f49cf9466f1d1755bc4157343edc957
Alexandre de Lima <allima1991@gmail.com>
2015-11-15 21:22:23 -0200

    hg

--- FormHome/FormProprietario.Designer.cs
+++ FormHome/FormProprietario.Designer.cs
@@ -124,7 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 375);
+            this.ClientSize = new System.Drawing.Size(847, 373);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormProprietario";
             this.Text = "FormProprietario";
