859828824f0fd3626b1ae766aafc6ebc5a8f2640
Alexandre de Lima <allima1991@gmail.com>
2015-11-16 00:13:05 -0200

    operação aluguel

--- FormHome/controle/AluguelOperacoes.cs
+++ FormHome/controle/AluguelOperacoes.cs
@@ -19,7 +19,7 @@ namespace FormHome.controle
         {
 
             SqlConnection conexao = new SqlConnection(stringConexao); // Define a conexao com o banco de dados. 
-            string comando = "insert Aluguel values (" + // Instruc5o SQL a ser executada.
+            string comando = "insert aluguel values (" + // Instruc5o SQL a ser executada.
             aluguel.Imovel.Id + "'," +
             aluguel.Inquilino.Id + "'," +
             aluguel.Corretor.Id + "')";
@@ -28,8 +28,34 @@ namespace FormHome.controle
             SqlCommand sql = new SqlCommand(comando, conexao);
             sql.ExecuteNonQuery(); // Executa a instrucio SQL no banco de dados. 
             conexao.Close(); // Fecha a conexao com o banco de dados. 
+        }
+        
+        public static void Alterar(Aluguel aluguel)
+        {
+            SqlConnection conexao = new SqlConnection(stringConexao); // Define a conexao com o banco de dados. 
+            string comando = "update aluguel set " + // Instrucao SQL a ser executada. 
+                "id_imovel = '" + aluguel.Imovel.Id + "', " +
+                "id_inquilino = '" + aluguel.Inquilino.Id + "'" +
+                "id_corretor = '" + aluguel.Corretor.Id + "', " +
+                "where id = " + aluguel.Id;
+            conexao.Open(); // Abre a conexao com o banco de dados.
+                            // Determina a instrucao SQL e a string de conexao a ser usada.
+            SqlCommand sql = new SqlCommand(comando, conexao);
+            sql.ExecuteNonQuery(); // Executa a instrucao SQL no banco de dados. 
+            conexao.Close(); // Fecha a conexao com o banco de dados. 
+        }
 
-
+        public static void Excluir(Aluguel aluguel)
+        {
+            SqlConnection conexao = new SqlConnection(stringConexao); // Define a conexao com o banco de dados.
+            string comando = "delete from aluguel where id = " +  // Instrucao SQL a ser executada. 
+                aluguel.Id.ToString();
+            conexao.Open(); // Abre a conexao com o banco de dados. 
+                            // Determina a instruc5o SQL e a string de conexao a ser usada.
+            SqlCommand sql = new SqlCommand(comando, conexao);
+            sql.ExecuteNonQuery(); // Executa a instrucao SQL no banco de dados. 
+            conexao.Close(); // Fecha a conexao com o banco de dados. 
+            
         }
     }
 }
