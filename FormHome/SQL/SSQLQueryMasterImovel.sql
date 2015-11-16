cbdc4db0cbeca352cfae55cf04f0bd03bac06a83
Alexandre de Lima <allima1991@gmail.com>
2015-11-16 20:22:03 -0200

    normalização do banco

--- FormHome/SQL/SSQLQueryMasterImovel.sql
+++ FormHome/SQL/SSQLQueryMasterImovel.sql
@@ -1,8 +1,7 @@
-﻿
 create database masterimovel;
 use masterimovel;
 
-create table corretor
+create table pessoa
 (
 id int identity not null primary key,
 nome varchar(250) not null,
@@ -11,31 +10,13 @@ endereco  varchar(250) not null,
 cpf varchar(25) not null,
 rg varchar(25) not null,
 telefone varchar(25) not null,
+tipo char(1)not null default('P') 
 );
-create table  inquilino
-(
-id int identity not null primary key,
-nome varchar(250) not null,
-email varchar(250) not null,
-endereco  varchar(250) not null,
-cpf varchar(25) not null,
-rg varchar(25) not null,
-telefone varchar(25) not null,
-);
+
 create table imovel
 (
 id int identity not null primary key,
-decricao varchar(250) not null,
-);
-create table proprietario
-(
-id int identity not null primary key,
-nome varchar(250) not null,
-email varchar(250) not null,
-endereco  varchar(250) not null,
-cpf varchar(25) not null,
-rg varchar(25) not null,
-telefone varchar(25) not null,
+decricao varchar(250) not null
 );
 
 create table proprietario_imovel
@@ -44,7 +25,7 @@ id int identity not null primary key,
 id_imovel int not null,
 id_proprietario int not null,
 constraint proimoimovel_fk foreign key(id_imovel) references imovel(id),
-constraint proimoproprietario_fk foreign key(id_proprietario) references proprietario(id)
+constraint proimoproprietario_fk foreign key(id_proprietario) references pessoa(id)
 );
 
 create table locacao(
@@ -53,6 +34,6 @@ id_imovel int not null,
 id_inquilino int not null,
 id_corretor int not null,
 constraint locimovel_fk foreign key(id_imovel) references imovel(id),
-constraint locinquilino_fk foreign key(id_inquilino) references inquilino(id),
-constraint loccorretor_fk foreign key(id_corretor) references corretor(id)
+constraint locinquilino_fk foreign key(id_inquilino) references pessoa(id),
+constraint loccorretor_fk foreign key(id_corretor) references pessoa(id)
 );
