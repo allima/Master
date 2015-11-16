3f62e66d573a659b265b6bde50b6a6bc11f29b91
Alexandre de Lima <allima1991@gmail.com>
2015-11-15 23:49:45 -0200

    incluir

--- FormHome/controle/AluguelOperacoes.cs
+++ FormHome/controle/AluguelOperacoes.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FormHome.model;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,25 @@ namespace FormHome.controle
 {
     class AluguelOperacoes
     {
+        private static string stringConexao = ConfigurationManager.
+            ConnectionStrings[@"FormHome.Properties.Settings.masterimovelConnectionString"].
+            ConnectionString;
+
+        public static void Incluir(Aluguel aluguel)
+        {
+
+            SqlConnection conexao = new SqlConnection(stringConexao); // Define a conexao com o banco de dados. 
+            string comando = "insert Aluguel values (" + // Instruc5o SQL a ser executada.
+            aluguel.Imovel.Id + "'," +
+            aluguel.Inquilino.Id + "'," +
+            aluguel.Corretor.Id + "')";
+            conexao.Open(); // Abre a conexao com o banco de dados.
+                            // Determina a instruc5o SQL e a string de conexio a ser usada. 
+            SqlCommand sql = new SqlCommand(comando, conexao);
+            sql.ExecuteNonQuery(); // Executa a instrucio SQL no banco de dados. 
+            conexao.Close(); // Fecha a conexao com o banco de dados. 
+
+
+        }
     }
 }
--- FormHome/model/Aluguel.cs
+++ FormHome/model/Aluguel.cs
@@ -8,5 +8,62 @@ namespace FormHome.model
 {
     class Aluguel
     {
+
+        private int id;
+        private Corretor corretor;
+        private Imovel imovel;
+        private Inquilino inquilino;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        internal Corretor Corretor
+        {
+            get
+            {
+                return corretor;
+            }
+
+            set
+            {
+                corretor = value;
+            }
+        }
+
+        internal Imovel Imovel
+        {
+            get
+            {
+                return imovel;
+            }
+
+            set
+            {
+                imovel = value;
+            }
+        }
+
+        internal Inquilino Inquilino
+        {
+            get
+            {
+                return inquilino;
+            }
+
+            set
+            {
+                inquilino = value;
+            }
+        }
     }
 }
